18186acc8390ceec9b510cb51e6163887dbe1f4c
Md. Alamin <mdalaminislam7878@gmail.com>
2022-01-13 01:28:04 +0600

    /add route

--- index.js
+++ index.js
@@ -19,12 +19,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(
-    cors({
-        "origin": "*",
-        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-        "preflightContinue": false,
-        "optionsSuccessStatus": 204
-      })
+    cors()
 )
 app.get('/', (req, res) => {
     res.json({user: "hello"});
