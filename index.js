f4ea175676f901a5057020f450796b5e9564bc12
Md. Alamin <mdalaminislam7878@gmail.com>
2022-01-13 01:05:53 +0600

    /add route

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(passport.session())
 
 app.use(
     cors({
-        origin: "https://mdalaminislam7878-social-app.netlify.app/",
+        origin: "*",
         methods: "GET, POST, PUT, DELETE",
         credentials: true
     })
